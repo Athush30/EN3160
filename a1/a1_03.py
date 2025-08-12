780a63c5eab47cb28fb3c75bb9586128682a3b1b
Thusajiny Ahilakumaran <ahilakumarant.22@uom.lk>
2025-08-12 14:26:03 +0530

    Create a1_03.py

---
+++ a1/a1_03.py
@@ -0,0 +1,88 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import image
+image = cv.imread('/content/Screenshot 2025-08-12 073137.png')
+assert image is not None
+img_lab = cv.cvtColor(image, cv.COLOR_BGR2LAB)
+img_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+# Split the image in to L*a*b* color space
+L,a,b = cv.split(img_lab)
+
+# Define and apply gamma transformation
+gamma = 0.75
+t = np.array([(i/255.0)**(gamma)*255 for i in np.arange(0, 256)]).astype('uint8')
+L_modified = cv.LUT(L, t)
+
+# Merge the channels
+merged = cv.merge([L_modified, a, b])
+img_modified = cv.cvtColor(merged, cv.COLOR_LAB2RGB)
+
+# Display the image
+plt.figure(figsize=(5, 5))
+plt.imshow(img_modified)
+plt.axis('off')
+plt.show()
+
+# Compute the histograms for both images
+hist1 = cv.calcHist([L], [0], None, [256], [0, 256])
+hist2 = cv.calcHist([L_modified], [0], None, [256], [0, 256])
+
+L_flat = L.flatten()
+L_modified_flat = L_modified.flatten()
+
+# Create a figure with two subplots
+plt.figure(figsize=(10, 5))
+
+# First subplot: Histogram of the first image
+plt.subplot(1, 2, 1)
+plt.hist(L_flat, bins=256, range=(0, 256), color='black', alpha=0.9)
+plt.title('Original L plane')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.xlim([0, 256])
+
+# Second subplot: Histogram of the second image
+plt.subplot(1, 2, 2)
+plt.hist(L_modified_flat, bins=256, range=(0, 256), color='black', alpha=0.9)
+plt.title('Gamma corrected L plane')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.xlim([0, 256])
+
+# Display the plots side by side
+plt.tight_layout()  # Adjusts the spacing between subplots for a neat layout
+plt.show()
+
+
+# Color channels
+colors = ('r', 'g', 'b')  # Color channels
+plt.figure(figsize=(10, 5))
+
+# First image's histograms
+plt.subplot(1, 2, 1)
+for i, col in enumerate(colors):
+    # Flatten the channel
+    channel_flat = img_rgb[:, :, i].flatten()  # Extract and flatten the red, green, or blue channel
+    plt.hist(channel_flat, bins=256, range=(0, 256), color=col, alpha=0.6)  # Use plt.hist
+plt.title('Original image')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.xlim([0, 256])
+
+# Second image's histograms
+plt.subplot(1, 2, 2)
+for i, col in enumerate(colors):
+    # Flatten the channel
+    channel_flat = img_modified[:, :, i].flatten()  # Extract and flatten the channel
+    plt.hist(channel_flat, bins=256, range=(0, 256), color=col, alpha=0.6)  # Use plt.hist
+plt.title('Gamma corrected image')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.xlim([0, 256])
+
+# Display the plots side by side
+plt.tight_layout()  # Adjusts the spacing between subplots for a neat layout
+plt.show()
