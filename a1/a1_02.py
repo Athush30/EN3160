3b152a5d2ad611d03a03839d9067f35853c02202
Thusajiny Ahilakumaran <ahilakumarant.22@uom.lk>
2025-08-12 15:47:30 +0530

    Update a1_02.py

--- a1/a1_02.py
+++ a1/a1_02.py
@@ -36,28 +36,25 @@ plt.imshow(g, cmap='gray', vmin=0, vmax=255)
 plt.axis('off')
 plt.show()
 
-
-
-
 #Grey matter
 #importing image
 image = cv.imread('/content/Screenshot 2025-08-12 071150.png', cv.IMREAD_GRAYSCALE)
 assert image is not None
 
-#gaussian pulse for white matter
+#gaussian pulse for grey matter
 mu=200
 sigma=10
 x = np.linspace(0, 255, 256)
-t = 255 * np.exp(-((x - mu)**2) / (2 * sigma**2))
+gaussian_grey = 255 * np.exp(-((x - mu)**2) / (2 * sigma**2))
 
 # Ensure the output is in the valid range for image intensities [0, 255]
-t = np.clip(t, 0, 255)
+gaussian_grey = np.clip(gaussian_grey, 0, 255)
 
-print(t.shape)
+print(gaussian_grey.shape)
 
 #plot the array
 plt.figure(figsize=(5, 5))
-plt.plot(t)
+plt.plot(gaussian_grey)
 plt.xlabel("Input intensity")
 plt.xlim(0, 255)
 plt.ylim(0, 255)
@@ -65,7 +62,7 @@ plt.ylabel("Output intensity")
 plt.grid(True)
 plt.show()
 
-g = t[image]
+g = gaussian_grey[image]
 
 # Display the image
 plt.figure(figsize=(5, 5))
